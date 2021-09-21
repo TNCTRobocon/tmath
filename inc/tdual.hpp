c0fed74a8d7baceaad25d3b78d4693a7fe23c8f1
teru kawakami <teru@soum.co.jp>
2021-09-21 17:45:37 +0000

    tdual.hpp: [wip] add dual function for autodiff

--- inc/tdual.hpp
+++ inc/tdual.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <functional>
+
 namespace tmath {
 
 template <class T> struct dual final {
@@ -70,5 +72,34 @@ constexpr const dual<T1> &operator/=(dual<T1> &x, const dual<T2> &y) {
     x.re = re, x.im = im;
     return x;
 }
+
+template <class T> struct dual_func final {
+    using func = std::function<T(const T &)>;
+    func re, im;
+    constexpr dual_func(func _re, func _im) : re(_re), im(_im) {}
+    constexpr dual_func(const func &_re, const func &_im) : re(_re), im(_im) {}
+    constexpr dual_func(func &&_re, func &&_im) : re(_re), im(_im) {}
+    constexpr dual_func(const dual_func &) = default;
+    constexpr dual_func(dual_func &&) = default;
+    T operator()(const T &x) const { return re(x); }
+};
+
+template <class T1> constexpr auto operator+(const dual_func<T1> &x) {
+    return x;
+}
+
+template <class T1> constexpr auto operator-(const dual_func<T1> &x) {
+    return dual_func<T1>([re = x.re](const T1 &x) { return -re(x); },
+                         [im = x.im](const T1 &x) { return -im(x); });
+}
+
+template <class T1> constexpr auto operator~(const dual_func<T1> &x) {
+    return dual_func<T1>(x.re, [im = x.im](const T1 &x) { return -im(x); });
+}
+
+// template <class T1, class T2> constexpr auto operator~(const dual_func<T1>
+// &x) {
+//     return dual<decltype(-x.re)>(x.re, -x.im);
+// }
 
 } // namespace tmath
